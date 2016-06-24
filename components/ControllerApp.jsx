78c2cf1bb57c87bf894e78cf7b3a6cca99e0e2f6
kuniiskywalker <kuniiskywalker@gmail.com>
2016-06-24 15:18:55 +0900

    add コントローラーにタブを実装

--- components/ControllerApp.jsx
+++ components/ControllerApp.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import SearchVideo from '../containers/SearchVideo'
 import VisibleChannelList from '../containers/VisibleChannelList'
 import VisibleVideoList from '../containers/VisibleVideoList'
+import {Tabs, TabLink, TabContent} from 'react-tabs-redux'
 
 const ControllerApp = () => (
     <div>
-        <SearchVideo />
-        <VisibleChannelList />
+        <Tabs>
+            <TabLink to="tab1">キーワード検索</TabLink>
+            <TabLink to="tab2">チャンネルリスト</TabLink>
+
+            <TabContent for="tab1"><SearchVideo /></TabContent>
+            <TabContent for="tab2"><VisibleChannelList /></TabContent>
+        </Tabs>
+
         <VisibleVideoList />
     </div>
 )
