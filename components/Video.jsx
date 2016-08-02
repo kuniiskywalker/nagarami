55372e88b499660d8fd2d79ef664c18bbe46542d
kuniiskywalker <kuniiskywalker@gmail.com>
2016-08-02 11:58:36 +0900

    add 動画検索結果にイイネ数・よくないね数を追加

--- components/Video.jsx
+++ components/Video.jsx
@@ -1,21 +1,61 @@
 import React, { PropTypes } from 'react';
 
 import {GridList, GridTile} from 'material-ui/GridList';
+import Divider from 'material-ui/Divider';
 
 import numeral from 'numeral';
 
-const Video = ({ id, title, thumbnail, description, viewCount, onPlayVideoClick, onStartPreviewVideoHover, onStopPreviewVideoHover }) => (
-    <GridTile
-        key={id}
-        title={title}
-        subtitle={<span>視聴回数 {numeral(viewCount).format('0,0')} 回</span>}
-        onClick={onPlayVideoClick}
-        onMouseOver={onStartPreviewVideoHover}
-        onMouseOut={onStopPreviewVideoHover}
-    >
-    <img src={thumbnail} />
-    </GridTile>
-)
+import ActionThumbUp from 'material-ui/svg-icons/action/thumb-up';
+import ActionThumbDown from 'material-ui/svg-icons/action/thumb-down';
+
+import {black, white} from 'material-ui/styles/colors';
+
+const styles = {
+    viewCounter: {
+        marginTop: "10px",
+        backgroundColor: black,
+        fontSize: 12
+    },
+    upDown: {
+        container: {
+            display: "flex",
+            "justify-content": "flex-end"
+        },
+        item: {
+            marginRight: 10
+        },
+        icon: {
+            marginRight: 5
+        }
+    }
+};
+
+const Video = ({ id, title, thumbnail, description, viewCount, likeCount, dislikeCount, onPlayVideoClick, onStartPreviewVideoHover, onStopPreviewVideoHover }) => {
+    
+    const subtitle = (
+        <div>
+            <div style={styles.viewCounter}>視聴回数 {numeral(viewCount).format('0,0')} 回</div>
+            <Divider />
+            <div style={styles.upDown.container}>
+                <div style={styles.upDown.item}><ActionThumbUp color={white} style={styles.upDown.icon} />{numeral(likeCount).format('0,0')}</div>
+                <div style={styles.upDown.item}><ActionThumbDown color={white} style={styles.upDown.icon} />{numeral(dislikeCount).format('0,0')}</div>
+            </div>
+        </div>
+    )
+    
+    return (
+        <GridTile
+            key={id}
+            title={title}
+            subtitle={subtitle}
+            onClick={onPlayVideoClick}
+            onMouseOver={onStartPreviewVideoHover}
+            onMouseOut={onStopPreviewVideoHover}
+        >
+            <img src={thumbnail} />
+        </GridTile>
+    )
+}
 
 Video.propTypes = {
     id: PropTypes.string.isRequired,
@@ -23,6 +63,8 @@ Video.propTypes = {
     thumbnail: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     viewCount: PropTypes.string.isRequired,
+    likeCount: PropTypes.string.isRequired,
+    dislikeCount: PropTypes.string.isRequired,
     onPlayVideoClick: PropTypes.func.isRequired,
     onStartPreviewVideoHover: PropTypes.func.isRequired,
     onStopPreviewVideoHover: PropTypes.func.isRequired
