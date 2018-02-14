d3368e921b6b4ba4450d6f63258da8c263bbd83e
kuniiskywalker <kuniiskywalker@gmail.com>
2018-02-14 11:43:46 +0900

    fix win installer

--- windows-build-installer.js
+++ windows-build-installer.js
@@ -1,11 +1,20 @@
 /* eslint no-console:0 */
 var winstaller = require('electron-winstaller');
 
+const package = require("./source/package.json");
+
 winstaller.createWindowsInstaller({
   appDirectory: './dist/nagarami-win32-x64/',
-  outputDirectory: '/tmp/build/installer64',
-  authors: 'kuniiskywalker.',
-  exe: 'nagarami.exe'
+  outputDirectory: './tmp/build/installer64',
+  title: 'nagarami',
+  authors: 'kuniiskywalker',
+  exe: 'nagarami.exe',
+  description: 'My App',
+  iconUrl: 'https://raw.githubusercontent.com/kuniiskywalker/nagarami/develop/source/icon.ico',
+  loadingGif: __dirname + '/loading.gif',
+  setupIcon: __dirname + '/source/icon.ico',
+  setupExe: 'nagarami-Setup-' + package["version"] + '.exe',
+  noMsi: true
 })
   .then(() => console.log('It worked!'))
   .catch(e => console.log(`No dice: ${e.message}`));
